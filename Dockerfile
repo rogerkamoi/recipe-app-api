e0c80f260600251a1b67ed0d70f36520c4bbd8a0
unknown <roger.kamoi@gmail.com>
2021-04-01 00:39:08 -0300

    Added custom user model

--- Dockerfile
+++ Dockerfile
@@ -3,12 +3,14 @@ MAINTAINER SJC App Dev Ltd
 
 ENV PYTHONUNBUFFERED 1
 
+# Install dependecies
 COPY ./requirements.txt /requirements.txt
 RUN pip install -r /requirements.txt
 
+# Setup directory Structure
 RUN mkdir /app
 WORKDIR /app
-COPY ./app /app
+COPY ./app/ /app
 
 RUN adduser -D user
 USER user
--- app/core/models.py
+++ app/core/models.py
@@ -1,14 +1,26 @@
 from django.db import models
-from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, \
+                                        PermissionsMixin
 
 
-class UseManager(BaseUserManager):
+class UserManager(BaseUserManager):
 
     def create_user(self, email, password=None, **extra_fields):
         """Creates and saves a new user"""
-        user = self.model(email=email, **extra_fields)
+        if not email:
+            raise ValueError('Users must have an email address')
+        user = self.model(email=self.normalize_email(email), **extra_fields)
         user.set_password(password)
-        user.save(uaing=self._db)
+        user.save(using=self._db)
+
+        return user
+
+    def create_superuser(self, email, password):
+        """Creates and saves a new superuser"""
+        user = self.create_user(email, password)
+        user.is_staff = True
+        user.is_superuser = True
+        user.save(using=self._db)
 
         return user
 
