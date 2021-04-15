4f6b4cdc78f1d71c77ca56f4c657dfe23802af11
unknown <roger.kamoi@gmail.com>
2021-04-15 00:36:33 -0300

    Added endpoint for creating and authenticating users

--- app/core/tests/test_commands.py
+++ app/core/tests/test_commands.py
@@ -2,7 +2,7 @@ from unittest.mock import patch
 
 from django.core.management import call_command
 from django.db.utils import OperationalError
-from djanfo.test import TestCase
+from django.test import TestCase
 
 
 class CommandTests(TestCase):
@@ -18,7 +18,7 @@ class CommandTests(TestCase):
     @patch('time.sleep', return_value=True)
     def test_wait_for_db(self, ts):
         """Test waiting for db"""
-        
+
         with patch('django.db.utils.ConnectionHandler.__getitem__') as gi:
             gi.side_effect = [OperationalError] * 5 + [True]
             call_command('wait_for_db')
